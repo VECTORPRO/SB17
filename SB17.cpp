019221093209d10bb90b4c809db2a74e945f7372
VECTORPRO <Vector59rus@gmail.com>
2022-02-09 08:23:28 +0300

    Сделал в формате sqrt(pow(x1, 2) + pow(y1, 2) + pow(z1, 2)), теперь только значение 0 возвращает почему то

--- SB17.cpp
+++ SB17.cpp
@@ -25,26 +25,15 @@ public:
 	Vector(double _x1, double _y1, double _z1) : x1(_x1), y1(_y1), z1(_z1)
 	{}
 	
-	double lengthVectorAB();
-
-	double SummAB()
+	double VectorLength()
 	{
-		SummAB = (pow(x2 - x1, 2) + (pow(y2 - y1, 2) + (pow(z2 - z1, 2));
-		return SummAB.sqrt();
-	};
-
-	void ShowvA()
-	{
-		std::cout << x1 << ", " << y1 << ", " << z1 << ", " << "\n";
+		return sqrt(pow(x1, 2) + pow(y1, 2) + pow(z1, 2));
 	}
 
 private:
-	double x1 = 120;
-	double y1 = 96;
-	double z1 = 1024;
-	double x2 = 730;
-	double y2 = 236;
-	double z2 = 2210;
+	double x1 = 12;
+	double y1 = 9;
+	double z1 = 10;
 };
 
 
@@ -56,13 +45,13 @@ int main()
 	std::cout << "Task 1 My New Class and integer value = 188: " << "\n";
 	std::cout << temp.GetA() << "\n";
 	std::cout <<  "\n";
+	
+	std::cout << "Task 2 Vector 3 values Point A = 0, 0, 0, Point B = 12, 9, 10: " << "\n";
+	
+	Vector Length;
+	Length.VectorLength();
 
-	Vector vA(120, 96, 1024);
-	Vector vB(730, 236, 2210);
-	
-	std::cout << "Task 2 Vector 3 values Point A = 120, 96, 1024, Point B = 730, 236, 2210: " << "\n";
-	
-	std::cout << vA.lengthVectorAB() << "\n";
 	std::cout << "\n";
-
+	std::cout << Length.VectorLength() <<"\n";
+	std::cout << "\n";
 }
